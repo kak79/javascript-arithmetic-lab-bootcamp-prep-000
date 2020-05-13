117a3e83c9917b1034aaf3c2659c2fe730c94c1f
Kimberly A Kohel <kohelka@gmail.com>
2020-05-13 14:20:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,58 @@
+function add(a) {
+  a += a
+}
+
+function subtract(a) {
+  a -= a
+}
+
+function multiply(a) {
+  a *= a
+}
+
+function divide(a) {
+  a /= a
+}
+
+function add(a,b) {
+  return a += b
+}
+
+function subtract(a,b) {
+return  a -= b
+}
+
+function multiply(a,b) {
+return  a *= b
+}
+
+function divide(a,b) {
+return  a /= b
+}
+
+function increment(n) {
+return  n += 1
+}
+
+function decrement(n) {
+return  n -= 1
+}
+
+function makeInt(n,radix) {
+  if (parseInt(n)) {
+     return n
+  }
+  else if (parseInt(n,radix=== 0)){
+     return n === 0
+  }
+  else (n ='sldkjflksjf')
+     return NaN
+}
+
+function preserveDecimal(n) {
+  if (parseFloat(n)){
+   return n
+  }
+  else 
+    return NaN
+}
